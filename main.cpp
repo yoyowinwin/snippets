02b683845f145d48484d978e42ee282ce6a7bbe7
wayne wang <wayne.wang@cloudminds.com>
2017-09-21 11:51:33 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ std::string runlengthEncode1(const std::string &in){
         cnt = 1;
         idx++;
     }
+    buf.append(1,c);
 
     return buf;
 }
@@ -40,28 +41,24 @@ std::string runlengthEncode2(const std::string &in){
     std::stringstream ss;
     ss<<in;
 
-    char head;
-    ss>>head;
-
-    char c = head;
-    int cnt = 1;
-    while (ss>>c) {
-        while(c == head && ss>>c) {
+    char a;
+    ss>>a;
+    /////////////////////////////
+    char b;
+    while (ss>>b) {
+        static int cnt = 1;
+        if(a!=b){
+            buf.append(1,a);
+            if(cnt>1) {
+                buf.append(std::to_string(cnt));
+            }
+            cnt = 1;
+            a=b;
+        } else {
             cnt++;
         }
-
-        if(!ss){
-            cnt++;
-        }
-        /////////////////////////////
-        buf.append(1,head);
-        if(cnt>1) {
-            buf.append(std::to_string(cnt));
-        }
-        /////////////////////////////
-        if(c!=head) head = c;
-        cnt = 1;
     }
+    buf.append(1,b);
 
     return buf;
 }
@@ -87,55 +84,50 @@ std::string runlengthDecode(const std::string &in){
 }
 
 void printParenthesesList(size_t n){
-    static std::vector<std::vector<std::string> > buf = {{"()"}};
-//    std::cout << std::endl<<std::string(50,'=')<<buf.size()<<std::endl;
+    static std::vector<std::vector<std::string> > buf = {{"9"},{"()"}};
+    std::cout << std::endl;
     if(n<1){
         std::cout <<""<<std::endl;
         return;
     } else if(n==1){
         std::cout <<"()"<<std::endl;
+        std::cout <<std::string(30,'=')<<1<<","<<1<<std::endl;
         return;
     }
-    if(buf.size() == n-1){
 
+    std::vector<std::string> buf_n;
+    if(buf.size() == n){
         /////////////////////////////
-        std::vector<std::string> buf_n;
-        for(size_t i=0;i<buf.at(n-2).size();++i){
-            std::string one = "(" + buf.at(n-2).at(i) + ")";
-            buf_n.push_back(one);
-            std::cout<< one<<std::endl;
-        }
-        /////////////////////////////
-        for(size_t i=0;i<n-2;++i){
+        for(size_t i=0;i<n;++i){
             for(size_t j=0;j<buf.at(i).size();++j){
-                for(size_t k=0;k<buf.at(n-2-i).size();++k){
-                    std::string one = buf.at(i).at(j).append(buf.at(n-2-i).at(k));
+                for(size_t k=0;k<buf.at(n-1-i).size();++k){
+                    std::string one = buf.at(i).at(j).append(buf.at(n-1-i).at(k));
+//                    if(j==0&&k==0) one = "("+one + ")";
                     buf_n.push_back(one);
                     std::cout<< one<<std::endl;
                 }
             }
         }
         buf.push_back(buf_n);
-        std::cout <<std::string(30,'=')<<buf_n.size()<<std::endl;
+        std::cout <<std::string(30,'=')<<buf.size()-1<<","<<buf_n.size()<<std::endl;
     }
 }
 
 int main()
 {
     {
-        std::string test = "aaasjjjjjffgddddsddffgrrrr";
+        std::string test = "aaasjjjjjffgddddsddffgrrrrf";
         std::cout <<test<<":\t"<< runlengthEncode1(test)<< std::endl;
     }
     {
-        std::string test = "aaasjjjjjffgddddsddffgrrrr";
+        std::string test = "aaasjjjjjffgddddsddffgrrrrf";
         std::cout <<test<<":\t"<< runlengthEncode2(test)<< std::endl;
     }
     {
-        std::string test = "a3sj5f2gd4sd2f2gr4";
+        std::string test = "a3sj5f2gd4sd2f2gr4f";
         std::cout <<test<<":\t"<< runlengthDecode(test)<< std::endl;
     }
 
-    for(size_t i=1;i<7;++i)
-        printParenthesesList(i);
+//    for(size_t i=1;i<4;++i)  printParenthesesList(i);
     return 0;
 }
